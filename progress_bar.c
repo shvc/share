3040f1b6f91fedee770efd69bfbf3107c39bc22d
dawter <dawter@qq.com>
2017-05-01 23:24:55 +0800

    optimization

--- progress_bar.c
+++ progress_bar.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/ioctl.h>
 
-#define LEN 102
+#define LEN 1024
 
 int main(int argc, char** argv)
 {
 	int i = 0;
 	char bar[LEN];
 	char str[] = "|\\-/";
+	struct winsize w;
 
 	memset(bar, 0, sizeof(bar));
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 
-	for( i=0; i<LEN; i++ ) {
+	for( i=0; i<w.ws_col-2; i++ ) {
 		bar[i] = '#';
-		printf("[%-101s][%d%%][%c]\r", bar, i, str[i%4]);
+		printf("[%-104s][%d%%][%c]\r", bar, i, str[i%4]);
 		fflush(stdout);
 		usleep(100000);
 	}
