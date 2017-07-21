b6a68211ec449e9a5f8c1c61435bf93b2c9891d2
dawter <dawter@qq.com>
2017-07-21 10:00:43 +0800

    add optimized select sort

--- sort.c
+++ sort.c
@@ -10,21 +10,6 @@ void output(int* a, size_t len)
 	return;
 }
 
-int* select_sort(int* a, size_t len)
-{
-	int i,j;
-	for(i=0; i<len-1; i++) {
-		for(j=i+1; j<len; j++) {
-			if(a[i] > a[j]) {
-				a[i] = a[i] ^ a[j];
-				a[j] = a[i] ^ a[j];
-				a[i] = a[i] ^ a[j];
-			}
-		}
-	}
-	return a;
-}
-
 int* bubble_sort(int* a, size_t len)
 {
 	int i,j;
@@ -39,6 +24,7 @@ int* bubble_sort(int* a, size_t len)
 	}
 	return a;
 }
+
 int* optimized_bubble_sort(int* a, size_t len)
 {
 	int i,j;
@@ -60,21 +46,55 @@ int* optimized_bubble_sort(int* a, size_t len)
 	return a;
 }
 
+int* select_sort(int* a, size_t len)
+{
+	int i,j;
+	for(i=0; i<len-1; i++) {
+		for(j=i+1; j<len; j++) {
+			if(a[i] > a[j]) {
+				a[i] = a[i] ^ a[j];
+				a[j] = a[i] ^ a[j];
+				a[i] = a[i] ^ a[j];
+			}
+		}
+	}
+	return a;
+}
+
+int* optimized_select_sort(int* a, size_t len)
+{
+	int i,j;
+	int min;
+	for(i=0; i<len-1; i++) {
+		min = i;
+		for(j=i+1; j<len; j++) {
+			if(a[min] > a[j]) {
+				min = j;
+			}
+		}
+		if(min != i) {
+			a[i]   = a[i] ^ a[min];
+			a[min] = a[i] ^ a[min];
+			a[i]   = a[i] ^ a[min];
+		}
+	}
+	return a;
+}
+
+
 int main(int argc, char** argv)
 {
-	int ret = 0;
+	int arry[] = {2,9,4,7,5,3,6,1,8,0};
+	size_t len = sizeof(arry)/sizeof(arry[0]);
 
-	int a[] = {2,9,4,7,5,3,6,1,8,0};
-	size_t len = sizeof(a)/sizeof(a[0]);
+	output(arry, len);
+	optimized_select_sort(arry, len);
+	output(arry, len);
+	optimized_bubble_sort(arry, len);
+	output(arry, len);
+	bubble_sort(arry, len);
+	output(arry, len);
 
-	output(a, len);
-	select_sort(a, len);
-	output(a, len);
-	optimized_bubble_sort(a, len);
-	output(a, len);
-	bubble_sort(a, len);
-	output(a, len);
-
-	return ret;
+	return 0;
 }
 
