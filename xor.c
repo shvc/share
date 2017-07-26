2623d90176a75dec6229ea041b282da234158166
dawter <dawter@qq.com>
2017-07-26 23:27:37 +0800

    M xor array

--- xor.c
+++ xor.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int print(int *arry, size_t len)
+void output(int *arry, size_t len)
 {
 	int i;
 	for(i=0; i<len; i++) {
@@ -22,12 +22,12 @@ int findodd(int *arry, size_t len)
 int main(int argc, char** argv)
 {
 	int ret = 0;
-	int arry[] = {1, 2, 3, 2, 1, 3, 1, 2};
-	print(arry, sizeof(arry)/sizeof(arry[0]));
-	ret = findodd(arry, sizeof(arry)/sizeof(arry[0]));
+	int arry[] = {3, 2, 3, 5, 1, 3, 1, 2, 5};
+	size_t len = sizeof(arry)/sizeof(arry[0]);
+	output(arry, len);
+	ret = findodd(arry, len);
 
 	printf("result: %d\n", ret);
-
 
 	return ret;
 }
