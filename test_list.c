415dd3f23a526fbb49eb919a93445c53cb1728a2
dawter <dawter@qq.com>
2017-07-26 08:26:45 +0800

    add function

--- test_list.c
+++ test_list.c
@@ -42,13 +42,36 @@ struct node* circular_list_init()
 	return head;
 }
 
-struct node* list_add(struct node* head, int data)
+struct node* list_add_node(struct node* head, int data)
 {
-	struct node* p = malloc(sizeof(struct node));
-	if(p && head) {
+	struct node* p;
+	if(NULL == head) {
+		return head;
+	}
+	p = malloc(sizeof(struct node));
+	if(p) {
 		p->data = data;
 		p->next = head->next;
 		head->next = p;
+	}
+	
+	return head;
+}
+
+struct node* list_add_nodes(struct node* head, size_t cnt)
+{
+	struct node* p = NULL;
+
+	if(NULL == head) {
+		return head;
+	}
+	for(; cnt>0; cnt--) {
+		p = malloc(sizeof(struct node));
+		if(p) {
+			p->data = cnt;
+			p->next = head->next;
+			head->next = p;
+		}
 	}
 	
 	return head;
@@ -97,34 +120,39 @@ struct node* list_reverse(struct node* pnode)
 
 struct node* list_delete_node(struct node** head, struct node* pnode)
 {
-#if 0
-	struct node* prev = *head;
-	struct node* walk = *head;
-	
-	while( walk && walk != pnode) {
-		prev = walk;
-		walk = walk->next;
-	}
-
-	if(walk == *head && walk == pnode) {
-		*head = walk->next;
-		free(walk);
-	}
-	else if(walk == pnode) {
-		prev->next = walk->next;
-		free(walk);
-	}
-#else
 	struct node **walk = head;
-
-#endif
+	
+	while(*walk && *walk != pnode) {
+		walk = &((*walk)->next);
+	}
+	if(*walk) {
+		*walk = pnode->next;
+		free(pnode);
+	}
 
 	return *head;
 }
 
+struct node* list_delete_nodes(struct node* head, int val)
+{
+	struct node **walk = &head;
+	struct node *tmp = NULL;
+
+	while(*walk) {
+		if((*walk)->data == val) {
+			tmp = *walk;
+			*walk = (*walk)->next;
+			free(tmp);
+		}
+		else {
+			walk = &((*walk)->next);
+		}
+	}
+	return head;
+}
+
 void list_free(struct node* head)
 {
-#if 0
 	struct node *p = head;
 	struct node *q = head;
 	if( NULL == head ) {
@@ -136,9 +164,6 @@ void list_free(struct node* head)
 		p = p->next;
 		free(q);
 	} while (p && p != head);
-#else
-	while(head = list_delete_node(&head, head));
-#endif
 
 }
 
@@ -152,12 +177,16 @@ int main(int argc, char** argv)
 	list_head = list_reverse(list_head);
 	list_output(list_head);
 
-	list_add(list_head, 1);
-	list_add(list_head, 2);
-	list_add(list_head, 3);
+	list_add_node(list_head, 1);
+	list_add_node(list_head, 2);
+	list_add_nodes(list_head, 3);
+	list_add_node(list_head, 2);
+	list_add_node(list_head, 2);
 	list_output(list_head);
 	printf("is_circular: %i\n", is_circular(list_head));
 	list_head = list_reverse(list_head);
+	list_output(list_head);
+	list_head = list_delete_nodes(list_head, 2);
 	list_output(list_head);
 
 	list_free(list_head);
@@ -165,13 +194,17 @@ int main(int argc, char** argv)
 	puts("------------------------------------");
 
 	circular_list_head = circular_list_init();
-	list_add(circular_list_head, 1);
-	list_add(circular_list_head, 2);
-	list_add(circular_list_head, 3);
+	list_add_node(circular_list_head, 1);
+	list_add_node(circular_list_head, 2);
+	list_add_nodes(circular_list_head, 3);
+	list_add_node(circular_list_head, 2);
+	list_add_node(circular_list_head, 2);
 	printf("is_circular: %i\n", is_circular(circular_list_head));
 	list_output(circular_list_head);
 	circular_list_head = list_reverse(circular_list_head);
 	printf("is_circular: %i\n", is_circular(circular_list_head));
+	list_output(circular_list_head);
+	circular_list_head = list_delete_nodes(circular_list_head, 2);
 	list_output(circular_list_head);
 
 	list_free(circular_list_head);
