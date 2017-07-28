e35db946fd98c4a3fede72b5cafe69988ae6b214
dawter <dawter@qq.com>
2017-07-28 08:21:57 +0800

    add comments

--- expression_parser.c
+++ expression_parser.c
@@ -3,6 +3,15 @@
 
 #define STACK_SIZE 32
 
+/*
+ * Step 1 − scan the expression from left to right 
+ * Step 2 − if it is an operand push it to stack 
+ * Step 3 − if it is an operator pull operand from stack and perform operation 
+ * Step 4 − store the output of step 3, back to stack 
+ * Step 5 − scan the expression until all operands are consumed 
+ * Step 6 − pop the stack and perform operation
+ */
+
 //char stack
 char stack[STACK_SIZE]; 
 int top = -1; 
