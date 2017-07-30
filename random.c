de383e6033210b97468e4645ce047ddf0a9d4cc6
dawter <dawter@qq.com>
2017-07-30 12:31:28 +0800

    add random function

--- random.c
+++ random.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
+#include <time.h>
 #include <stdlib.h>
 
 
+void random1(int length, int size)
+{
+	int i;
+	for(i=0; i<length; i++) {
+		printf("%d ", rand()%size);
+	}
+	puts("");
+}
+
+void random2(int length, int size)
+{
+	int i;
+	srandom(time(NULL));
+
+	for(i=0; i<length; i++) {
+		printf("%ld ", random()%size);
+	}
+	puts("");
+}
+
 int main(int argc, char** argv)
 {
-	int count = 10;
+	int length = 10;
+	random1(length, 100);
+	random2(length, 100);
 
-	while(count--) {
-		printf("%d\n", rand());
-	}
 
 	return 0;
 }
