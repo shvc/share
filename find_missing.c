f382a2dbb0b7118c095f5aadee06a58921f3b83a
root <root@cs7.carlos.com>
2017-08-13 20:10:19 +0800

    add file

---
+++ find_missing.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+ * Given an array contains N numbers of 0 .. N, find which number doesn't exist in the array.
+ */
+
+int find_missing(int array[], size_t len)
+{
+
+	return 0;
+}
+
+
+int main(int argc, char** argv)
+{
+	int retval = 0;
+	int array[] = {2, 9, 4, 7, 5, 3, 6, 8};
+	size_t len = sizeof(array)/sizeof(array[0]);
+
+	printf("missing: %d\n", find_missing(array, len);
+
+	return retval;
+
+}
