70876c79334349bda5501b7e32de7c4a6d88843e
dawter <dawter@qq.com>
2017-08-13 20:31:19 +0800

    M...

--- find_missing.c
+++ find_missing.c
@@ -4,21 +4,59 @@
  * Given an array contains N numbers of 0 .. N, find which number doesn't exist in the array.
  */
 
-int find_missing(int array[], size_t len)
+void output(int *array, size_t len)
+{
+	int i;
+	for(i=0; i<len; i++) {
+		printf("%2i,", array[i]);
+	}
+	puts("");
+
+}
+
+void swap(int *x, int *y)
+{
+	if( x != y) {
+		*x = *x ^ *y;
+		*y = *x ^ *y;
+		*x = *x ^ *y;
+	}
+}
+
+/* find missing number in 0...N array */
+int find_missing0(int array[], size_t len)
+{
+	int i;
+
+	for(i=0; i<len; i++) {
+		while(array[i] != i && array[i] < len)  swap(array+i, array+array[i]);
+	}
+
+	for(i=array[0]; i<len; i++) {
+		if(array[i] != i) return i;
+	}
+
+	return len;
+}
+
+
+/* find missing number in array */
+int find_missing1(int array[], size_t len)
 {
 
 	return 0;
 }
 
-
 int main(int argc, char** argv)
 {
 	int retval = 0;
-	int array[] = {2, 9, 4, 7, 5, 3, 6, 8};
+	int array[] = {2, 9, 4, 7, 5, 3, 6, 0, 8};
 	size_t len = sizeof(array)/sizeof(array[0]);
 
-	printf("missing: %d\n", find_missing(array, len);
+	output(array, len);
+	printf("missing: %d\n", find_missing0(array, len));
+	output(array, len);
 
 	return retval;
-
 }
+
