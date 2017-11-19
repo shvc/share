e08caa23a8f5b02b708152b885dc855272620fd4
dawter <dawter@qq.com>
2017-11-19 23:07:45 +0800

    add eval

--- dump_journal.sh
+++ dump_journal.sh
@@ -42,13 +42,12 @@ do
 	echo "id: $chunkid" > /dev/null
 	mkdir -p $chunkid
 	curl -s "http://${ipaddr}:9101/diagnostic/1/ShowChunkInfo?cos=${cos}&chunkid=${chunkid}" > $chunkid/${chunkid}.info
-	# get chunk primary vdc(zone)
-	zone=$(awk '/primary:/{print $2}' $chunkid/${chunkid}.info)
-	# get sealed time
-	sealedTime=$(awk '/sealedTime:/{print $2}' $chunkid/${chunkid}.info)
+	# get chunk primary(zone) and sealedTime
+	eval $(awk '/primary/{print "zone="$2};/sealedTime:/{print "sealedTime="$2}' $chunkid/${chunkid}.info)
+	echo "zone=$zone, sealedTime=$sealedTime"
 	# get chunk dtId
 	curl -s "http://${ipaddr}:9101/diagnostic/CT/1/DumpAllKeys/CHUNK?chunkId=${chunkid}&useStyle=raw" | grep -B1 schema > $chunkid/${chunkid}.dtId
-	dtId=$(awk '/http/{print $4}' $chunkid/${chunkid}.dtId)
+	eval $(awk '/http/{print "dtId="$4}' $chunkid/${chunkid}.dtId)
 	# List JR(Journal Region)
 	curl -s "http://${ipaddr}:9101/diagnostic/PR/2/DumpAllKeys/DIRECTORYTABLE_RECORD?type=JOURNAL_REGION&dtId=${dtId}&zone=${zone}&useStyle=raw&showvalue=gpb" | grep -B1 schema > $chunkid/${chunkid}.JR
 	# Dump JR content
